2df57fd7166ede93ed1d59bfe4c4f3a0db84adba
FROSTENDJP <frostendJP@gmail.com>
2026-09-11 20:40:00 -0300

    Update API URL in pagamentos-config.js

--- js/pagamentos-config.js
+++ js/pagamentos-config.js
@@ -3,7 +3,7 @@
  * Use a mesma URL do Apps Script ou uma implantação dedicada com aba Vendas.
  */
 const PAGAMENTOS_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwPZY2jq-iUF5CG7NMt8NBtuqd7EzePqCnN-9qUVGDANnY-o-d1nT64dQ8GXzTHjHM9/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxry3oarrIJ7vL7NB5hx8samceYMd6xk1z4uBwgd-C_xsNxkgKvevPa-Sg6bC5HdTS2/exec',
 
   AUTH: {
     analise: { label: 'Análise Comercial' },
